b7730a00d5976e46b7144ec74bb4d53f21e25dff
juliajakob370 <jjakob3896@conestogac.on.ca>
2026-04-29 19:40:39 -0400

    stats pageee

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import LandingPage from "./pages/public/[0]LandingPage";
-import Signup from "./pages/public/SignUp1";
-import Login from "./pages/public/[1]LoginPage";
+import LoginPage from "./pages/public/[1]LoginPage";
+import SignupPage from "./pages/public/SignUp1";
+
+import StatsPage from "./pages/dashboard/[5]StatsPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public pages */}
         <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
+        {/* Dashboard pages */}
+        <Route path="/dashboard" element={<StatsPage />} />
+        <Route path="/stats" element={<StatsPage />} />
+
+        {/* Coming soon pages */}
+        <Route path="/journal" element={<StatsPage />} />
+        <Route path="/pet-room" element={<StatsPage />} />
+        <Route path="/account" element={<StatsPage />} />
+        <Route path="/settings" element={<StatsPage />} />
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
