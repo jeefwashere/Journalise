a4ff78454d541437be72b4adf692621fda078ef0
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-04-29 09:52:12 -0400

    Added activity log model

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Activity(models.Model):
+    class Category(models.TextChoices):
+        WORK = "work", "Work"
+        STUDY = "study", "Study"
+        ENTERTAINMENT = "entertainment", "Entertainment"
+        COMMUNICATION = "communication", "Communication"
+        OTHER = "other", "Other"
+
+    title = models.CharField(max_length=255)
+    category = models.CharField(
+        max_length=30, choices=Category.choices, default=Category.OTHER
+    )
+    description = models.TextField(blank=True)
+    started_at = models.DateTimeField()
+    ended_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
