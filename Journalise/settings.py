0e9757ca2fe1c98f352f25d0e2788016bc87672d
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-04-28 22:38:44 -0400

    Created UserProfile model

--- Journalise/settings.py
+++ Journalise/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
+    "journal",
+    "pets",
+    "stats",
+    "summary",
+    "tracking",
 ]
 
 MIDDLEWARE = [
