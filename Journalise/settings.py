c5acd1d916f654d3858036e05b52e6d78390c43f
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-04-29 13:37:07 -0400

    Added proper database teardown

--- Journalise/settings.py
+++ Journalise/settings.py
@@ -115,16 +115,18 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DB_NAME"),
-            "USER": os.getenv("DB_USER"),
-            "PASSWORD": os.getenv("DB_PASSWORD"),
-            "HOST": os.getenv("DB_HOST"),
-            "PORT": os.getenv("DB_PORT"),
-            "OPTIONS": {
-                "sslmode": "require",
-            },
+            "NAME": os.getenv("DB_NAME", "postgres"),
+            "USER": os.getenv("DB_USER", "postgres"),
+            "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
+            "HOST": os.getenv("DB_HOST", "localhost"),
+            "PORT": os.getenv("DB_PORT", "5432"),
         }
     }
+
+    if os.getenv("DB_SSL", "False") == "True":
+        DATABASES["default"]["OPTIONS"] = {
+            "sslmode": "require",
+        }
 
 
 # Password validation
